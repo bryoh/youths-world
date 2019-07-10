9e5dc56148a0ee6043c30ffa442b69cfa875ebec
bryoh <brianbryo@gmail.com>
2019-07-10 14:46:49 +0100

    Get secret key  from env

--- app/app/settings/production.py
+++ app/app/settings/production.py
@@ -2,7 +2,8 @@ from .base import *
 import os
 
 ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost', 'youths-world.herokuapp.com']
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changemeplease')
+
 DEBUG = os.environ.get('DEBUG', False)
 
 try:
