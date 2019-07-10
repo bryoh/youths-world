524266d94fa4be03a3292d15524d1fcedff91320
bryoh <brianbryo@gmail.com>
2019-07-10 14:16:16 +0100

    Allowed hosts

--- app/app/settings/dev.py
+++ app/app/settings/dev.py
@@ -5,6 +5,7 @@ DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'vr8oz8=5^dw-bkmundzpej-_bkz%tasmwcrlq2i05dlv!hpel_'
+ALLOWED_HOSTS = ['*youths-world.herokuapp.com', 'localhost']
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
