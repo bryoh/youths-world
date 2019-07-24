cac1566c13586697600829fd236f922ee3423dbd
bryoh <brianbryo@gmail.com>
2019-07-24 14:06:16 +0100

    Integrated wagtail with django-oscar

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,31 +1,35 @@
+from django.apps import apps
+
+from django.urls import include, re_path, path  # > Django-2.0
 from django.conf import settings
-from django.conf.urls import include, url
 from django.contrib import admin
-
-from search import views as search_views
-
+from django.conf.urls.static import static
 from wagtail.admin import urls as wagtailadmin_urls
-from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
-from longclaw import urls as longclaw_urls
+from wagtail.core import urls as wagtail_urls
+from oscar.views import handler403, handler404, handler500
 
+
+admin.autodiscover()
 urlpatterns = [
-    url(r'^django-admin/', admin.site.urls),
-
-    url(r'^admin/', include(wagtailadmin_urls)),
-    url(r'^documents/', include(wagtaildocs_urls)),
-
-    url(r'^search/$', search_views.search, name='search'),
-
-    url(r'', include(longclaw_urls)),
-    url(r'', include(wagtail_urls))
+    path('i18n/', include('django.conf.urls.i18n')),  # > Django-2.0
+    re_path(r'^django-admin/', admin.site.urls),
+    re_path(r'^admin/', include(wagtailadmin_urls)),
+    path('', include(apps.get_app_config('oscar').urls[0])),  # > Django-2.0
+    re_path(r'^cms/', include(wagtailadmin_urls)),
+    re_path(r'^documents/', include(wagtaildocs_urls)),
+    re_path(r'^pages/', include(wagtail_urls)),
 ]
 
-
 if settings.DEBUG:
-    from django.conf.urls.static import static
-    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    import debug_toolbar
 
-    # Serve static and media files from development server
-    urlpatterns += staticfiles_urlpatterns()
+    # Server statics and uploaded media
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Allow error pages to be tested
+    urlpatterns += [
+        re_path(r'^403$', handler403, {'exception': Exception()}),
+        re_path(r'^404$', handler404, {'exception': Exception()}),
+        re_path(r'^500$', handler500),
+        re_path(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
