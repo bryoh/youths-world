648e43c4e5ad17de385073cbf7d72feaa37d26e8
bryoh <brianbryo@gmail.com>
2019-07-25 17:18:01 +0100

    Modified name and updated gitignore

--- app/app/settings.py
+++ app/app/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 from oscar.defaults import *
 
+OSCAR_SHOP_TAGLINE = 'Bikers World Automobiles'
+OSCAR_SHOP_NAME = 'Bombolulu'
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
